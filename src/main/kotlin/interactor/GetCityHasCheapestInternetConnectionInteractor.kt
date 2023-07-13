4b1c0abf0982d47e0bd645d30f4027f1185f74bc
AhmedSalahAamer <104269198+AhmedSalahAamer@users.noreply.github.com>
2023-07-13 20:55:55 +0300

    feature/cheapest-internet-connection

--- src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
@@ -6,10 +6,23 @@ class GetCityHasCheapestInternetConnectionInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(): CityEntity{
-        throw Throwable("Not Implemented yet")
+    fun execute(limit:Int): List<String>{
+        return dataSource
+            .getAllCitiesData()
+            .filter (::excludeNullSalariesAndLowQualityData)
+            .sortedByDescending (::getAverageInternetConnection)
+            .take(limit)
+            .map { it.cityName }
+
+
     }
 
-
+    private fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
+        return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
+    }
+    private fun getAverageInternetConnection(cityEntity:CityEntity):Float {
+        var result:Float = cityEntity.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl!! / cityEntity.averageMonthlyNetSalaryAfterTax!!
+        return result
+    }
 
 }
