6098609bbf4d2a88b1cda41736abd2fe95ff9f6e
amanymohamed137 <93156418+amanymohamed137@users.noreply.github.com>
2023-07-15 01:44:53 +0300

    Country Name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,24 @@
 import dataSource.CsvDataSource
 import dataSource.utils.CsvParser
-import fakedata.FakeCarbonatedTaxesOnCokeCountriesData
-import fakedata.FakeShortCarbonatedList
-import interactor.*
+import interactor.CostOfLivingDataSource
+import interactor.GetHighestSalaryAverageCitiesNamesInteractor
+import interactor.SearchForSalary
 
 fun main() {
     val csvParser = CsvParser()
     val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser)
 
+    val getHighestSalaryAverageCities = GetHighestSalaryAverageCitiesNamesInteractor(dataSource)
+    println(getHighestSalaryAverageCities.execute(limit = 10))
+    printSeparationLine()
+
+    val getAvargeSalaryForAcountry=SearchForSalary(dataSource)
+   println(getAvargeSalaryForAcountry.execute("egypt"))
+
 
 //    val getCityHasCheapestInternetConnectionInteractor = GetCityHasCheapestInternetConnectionInteractor(dataSource)
 //    println(getCityHasCheapestInternetConnectionInteractor.execute())
 
-    printSeparationLine()
-
-    val x= GetCountriesNamesWithHighestCokePrice(dataSource)
-    println(x.execute())
-    printSeparationLine()
-
-
-    val getHighestSalaryAverageCitiesNamesInteractor= GetHighestSalaryAverageCitiesNamesInteractor(dataSource)
-    println(getHighestSalaryAverageCitiesNamesInteractor.execute(4))
 }
 private fun printSeparationLine(){
     print("\n_______________________________\n")
