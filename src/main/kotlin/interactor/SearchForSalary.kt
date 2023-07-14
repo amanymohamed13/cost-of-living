d6e424f537eecec24c9b831dcc87f6f6ccf16e42
AhmedSalahAamer <104269198+AhmedSalahAamer@users.noreply.github.com>
2023-07-14 18:14:18 +0300

    feature/cheapest-internet-connection

---
+++ src/main/kotlin/interactor/SearchForSalary.kt
@@ -0,0 +1,60 @@
+package interactor
+
+import model.CityEntity
+import java.util.*
+
+class SearchForSalary(
+    private val dataSource: CostOfLivingDataSource,
+   private var Name:String
+){
+    init {
+        this.Name=correctTheCountryName(Name)
+    }
+    fun execute(limit:Int):List<Float> {
+//      val Name = correctTheCountryName(countryName)
+        return dataSource
+            .getAllCitiesData()
+            .filter (::excludeNullSalariesAndLowQualityData)
+            .filter (::excludeOtherCountries )
+            .map { it.averageMonthlyNetSalaryAfterTax!! }
+
+
+
+    }
+
+
+
+    private fun excludeNullSalariesAndLowQualityData(city: CityEntity): Boolean {
+        return city.averageMonthlyNetSalaryAfterTax != null && city.dataQuality
+    }
+    private fun excludeOtherCountries(city: CityEntity): Boolean {
+        return city.country == Name
+    }
+
+    private fun correctTheCountryName(countryName: String):String{
+//        for (i in countryName.indices-1){
+//            if (i == 0)
+//            {
+//                if (countryName[i]>='a' && countryName[i] <= 'z'){
+//                    countryName[i].uppercaseChar()
+//                }
+//            }
+//            else{
+//                if (countryName[i]>='A' && countryName[i] <= 'Z'){
+//                    countryName.lowercase()
+//                }
+//            }
+//        }
+//        return countryName
+
+
+        return countryName.replaceFirstChar {
+            if (it.isLowerCase())
+                it.titlecase(Locale.getDefault())
+            else
+                it.toString()
+        }
+
+    }
+
+}
